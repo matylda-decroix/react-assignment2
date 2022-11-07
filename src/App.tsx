8cde3a32dd3923ffd8cc607ac16233fc48d71233
MATYLDA DECROIX <matylda.decroix@gmail.com>
2022-11-07 16:35:14 +0100

    Fetching data with react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,56 @@
 import "./App.css";
+import { useQuery } from "@tanstack/react-query";
+
+interface User {
+  id: number;
+  username: string;
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+}
 
 function App() {
-  return <div>Hello</div>;
+  const { data, isLoading } = useQuery(
+    ["users"],
+    (): Promise<User[]> =>
+      fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+        res.json()
+      ),
+    {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    }
+  );
+  const users = data;
+  return (
+    <>
+      {isLoading === true && <div>Loading user data...</div>}
+      <div className="div">
+        {users?.map((user) => {
+          return (
+            <div key={user.id}>
+              <div>
+                <div>
+                  <img
+                    src={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}
+                  />
+                </div>
+                <div>
+                  <div>
+                    <h2>{user.name}</h2>
+                    <p>{user.email}</p>
+                    <p>{user.phone}</p>
+                    <p>{user.website}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default App;
